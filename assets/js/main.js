352283023be8fe77716786f17cb949fe1e1e6e12
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2022-12-10 21:13:35 -0500

    mejora-mtk-16

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,150 +1,148 @@
-      const productos = [
-          {
-            id: 1,
-            name: 'Hoodies',
-            price: 14.00,
-            image: 'assets/images/featured1.png',
-            category: 'hoodies',
-            quantity: 10
-          },
-          {
-            id: 2,
-            name: 'Shirts',
-            price: 24.00,
-            image: 'assets/images/featured2.png',
-            category: 'shirts',
-            quantity: 15
-          },
-          {
-            id: 3,
-            name: 'Sweatshirts',
-            price: 24.00,
-            image: 'assets/images/featured3.png',
-            category: 'shirts',
-            quantity: 20
-          }
-        ]
-
-
-        
-
-        let loadComponet = ()=>{
-
-          let loader = document.getElementById('load')
-          setTimeout(() => {
-            loader.classList.add('hide')
-          }, 4000);
-          
-        }
-        document.addEventListener('DOMContentLoaded',()=>{
-          loadComponet()
-        })
+const productos = [
+  {
+    id: 1,
+    name: 'Hoodies',
+    price: 14.00,
+    image: 'assets/images/featured1.png',
+    category: 'hoodies',
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: 'Shirts',
+    price: 24.00,
+    image: 'assets/images/featured2.png',
+    category: 'shirts',
+    quantity: 15
+  },
+  {
+    id: 3,
+    name: 'Sweatshirts',
+    price: 24.00,
+    image: 'assets/images/featured3.png',
+    category: 'shirts',
+    quantity: 20
+  }
+]
 
 
 
-        const body = document.body
-        const header = document.getElementById('header')
 
-        window.addEventListener('scroll',()=>{
-          if (scrollY >=55) {
-            header.classList.add('scroll-bg')
-          }else{
-            header.classList.remove('scroll-bg')
-          }
-        })
+let loadComponet = () => {
 
+  let loader = document.getElementById('load')
+  setTimeout(() => {
+    loader.classList.add('hide')
+  }, 4000);
 
-      const btnThemeSun = document.getElementById('btn-theme-change')
-      const btnThemaDark = document.getElementById('btn-theme-luna')
-      const btnCarShopping = document.getElementById('btn-car-shopping')
-      const containerCarShopping = document.getElementById('container-car-shopping')
-      const btnCloseCarShopping = document.getElementById('btn-car-shopping-close')
-
-        const darkThemeChange = ()=>{
-          body.classList.toggle('dark')
-          btnThemeSun.classList.toggle('desactive')
-          btnThemaDark.classList.toggle('active')
-        }
-        
-        btnThemeSun.addEventListener('click', () => darkThemeChange())
-        btnThemaDark.addEventListener('click', () => darkThemeChange())
-
-        btnCarShopping.addEventListener('click',()=> containerCarShopping.classList.add('position-cero'))
-        btnCloseCarShopping.addEventListener('click',()=> containerCarShopping.classList.remove('position-cero'))
+}
+document.addEventListener('DOMContentLoaded', () => {
+  loadComponet()
+})
 
 
-        // Home
 
-        //CREAR LOS ELEMETOS
-        const containerHome = document.querySelector('#container-home')
+const body = document.body
+const header = document.getElementById('header')
 
-       
-       
-       let contador = 0
-
-       setInterval(() => {
-        contador++
-        productos.filter(prod => {
-      
-          if (prod.id==contador) {
-             const fragmentHome = `<section class="container-imag-product-home">
-              <figure class="sub-container-imag-home">
-              <img src="${prod.image}" alt="" class="imag-prodctus-home">
-              </figure> 
-         </section>
-
-        <section class="container-description-product-home">
-            <h2 class="category-product">${prod.name}</h2>
-            <h1 class="title-pructo-home">Collections 2023</h1>
-            <p class="description-product-home">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate assumenda quod, molestiae officiis illum repellendus dolorum a ut illo velit reprehenderit laudantium impedit eum accusantium animi labore voluptatem doloribus distinctio?</p>
-            <h4 class="precio-product-home">$.${prod.price}.00</h4>
-            <div class="container-boton-addCar-home">
-                <figure class="bt-discover-home">Discover</figure>
-                <figure class="bt-addt-car-home">Add to Card</figure>
-            </div>
-        </section>`
-            
-            containerHome.innerHTML = fragmentHome
-
-          } else if (contador>3) {
-            contador = 0
-          }
-          
-        
-          
-        })
-
-       }, 5000);
-        
-   
+window.addEventListener('scroll', () => {
+  if (scrollY >= 55) {
+    header.classList.add('scroll-bg')
+  } else {
+    header.classList.remove('scroll-bg')
+  }
+})
 
 
-      /* ------  CONTAINER FILTER -------*/
+const btnThemeSun = document.getElementById('btn-theme-change')
+const btnThemaDark = document.getElementById('btn-theme-luna')
+const btnCarShopping = document.getElementById('btn-car-shopping')
+const containerCarShopping = document.getElementById('container-car-shopping')
+const btnCloseCarShopping = document.getElementById('btn-car-shopping-close')
+
+const darkThemeChange = () => {
+  body.classList.toggle('dark')
+  btnThemeSun.classList.toggle('desactive')
+  btnThemaDark.classList.toggle('active')
+}
+
+btnThemeSun.addEventListener('click', () => darkThemeChange())
+btnThemaDark.addEventListener('click', () => darkThemeChange())
+
+btnCarShopping.addEventListener('click', () => containerCarShopping.classList.add('position-cero'))
+btnCloseCarShopping.addEventListener('click', () => containerCarShopping.classList.remove('position-cero'))
 
 
-          const containerNavFilter = document.getElementById('container-filter')
-          
-          productos.forEach(prod =>{
-            const {id,name,quantity} = prod
+// Home
 
-            const fragmentFilter = `<aside id="container-item-filter" class="container-item-filter">
+//CREAR LOS ELEMETOS
+const containerHome = document.querySelector('#container-home')
+
+
+
+let contador = 0
+
+setInterval(() => {
+  contador++
+  productos.filter(prod => {
+
+    if (prod.id == contador) {
+      const fragmentHome = `
+             <section class="container-imag-product-home">
+                <figure class="sub-container-imag-home">
+                  <img src="${prod.image}" alt="" class="imag-prodctus-home">
+                </figure> 
+              </section>
+
+            <section class="container-description-product-home">
+                <h2 class="category-product">${prod.name}</h2>
+                <h1 class="title-pructo-home">Collections 2023</h1>
+                <p class="description-product-home">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate assumenda quod, molestiae officiis illum repellendus dolorum a ut illo velit reprehenderit laudantium impedit eum accusantium animi labore voluptatem doloribus distinctio?</p>
+                <h4 class="precio-product-home">$.${prod.price}.00</h4>
+                <div class="container-boton-addCar-home">
+                    <figure class="bt-discover-home">Discover</figure>
+                    <figure class="bt-addt-car-home">Add to Card</figure>
+                </div>
+            </section>`
+
+      containerHome.innerHTML = fragmentHome
+
+    } else if (contador > 3) {
+      contador = 0
+    }
+  })
+
+}, 5000);
+
+
+
+
+/* ------  CONTAINER FILTER -------*/
+
+
+const containerNavFilter = document.getElementById('container-filter')
+
+productos.forEach(prod => {
+  const { id, name, quantity } = prod
+
+  const fragmentFilter = `<aside id="container-item-filter" class="container-item-filter">
           <h5 class="title-item-filter" onclick="btnfiltrarProducto(${id})">${name}</h5>
           <h6 class="description-item-filter">Stock ${quantity}</h6>
         </aside>`
 
-          containerNavFilter.innerHTML += fragmentFilter
+  containerNavFilter.innerHTML += fragmentFilter
 
-          })
+})
 
 
 
-          // filtar por productos
-          function btnShowProduc(){
-            containerProdructos.innerHTML = ''
-            productos.forEach(produc =>{
-              const {id,name,price,image,quantity} = produc
-        
-              const fragmentItemProduct = `
+// filtar por productos
+function btnShowProduc() {
+  containerProdructos.innerHTML = ''
+  productos.forEach(produc => {
+    const { id, name, price, image, quantity } = produc
+
+    const fragmentItemProduct = `
               <div class="container-card">
                             <figure class="container-card-imag">
                                 <img src="${image}" alt="" class="imag-product-card">
@@ -158,16 +156,16 @@
                                 </div>
                             </article>
                         </div>`
-              containerProdructos.innerHTML += fragmentItemProduct
-            
-            })
-          }
+    containerProdructos.innerHTML += fragmentItemProduct
 
-          function btnfiltrarProducto(id) {
-            
-        productos.filter(prod =>{
-          if (prod.id===id) {
-                const fragmentItemProduct = `
+  })
+}
+
+function btnfiltrarProducto(id) {
+
+  productos.filter(prod => {
+    if (prod.id === id) {
+      const fragmentItemProduct = `
                       <div class="container-card">
                       <figure class="container-card-imag">
                           <img src="${prod.image}" alt="" class="imag-product-card">
@@ -181,39 +179,41 @@
                           </div>
                       </article>
                   </div>`
-        containerProdructos.innerHTML = fragmentItemProduct 
-              }
-            
-          }) 
-          
-      }
+      containerProdructos.innerHTML = fragmentItemProduct
+    }
+
+  })
+
+}
 
 
-          
 
 
-        /* ------  CONTAINER Productos-------*/
-        let carShopping =[]
-        
-      const containerProdructos = document.querySelector('#container-productos')
-      const cantidadProduCar = document.querySelector('#cantidad-produc-car')
-      const precioTotal = document.querySelector('#precio-total')
 
-      document.addEventListener('DOMContentLoaded',()=>{
-            carShopping = JSON.parse(localStorage.getItem('carrito')) || []
-            mostrarProductosCarShopping()
-      })
+/* ------  CONTAINER Productos-------*/
+let carShopping = []
 
-      productos.forEach(produc =>{
-        const {id,name,price,image,quantity} = produc
+const containerProdructos = document.querySelector('#container-productos')
+const cantidadProduCar = document.querySelector('#cantidad-produc-car')
+let precioTotal = document.querySelector('#precio-total')
 
-        const fragmentItemProduct = `
+let total = 0
+
+document.addEventListener('DOMContentLoaded', () => {
+  carShopping = JSON.parse(localStorage.getItem('carrito')) || []
+  mostrarProductosCarShopping()
+})
+
+productos.forEach(produc => {
+  const { id, name, price, image, quantity } = produc
+
+  const fragmentItemProduct = `
         <div class="container-card">
                       <figure class="container-card-imag">
                           <img src="${image}" alt="" class="imag-product-card">
                       </figure>
                       <article class="container-descrip-produc-card">
-                          <button class="btn-add-card" onclick="addProductCard(${id})">+</button>
+                          <button id="${id}" class="btn-add-card">+</button>
                           <h4 class="name-product-card">${name}</h4>
                           <div class="container-precio-produc-card">
                               <h3 class="precio-product-card">$.${price}.00</h3>
@@ -221,64 +221,61 @@
                           </div>
                       </article>
                   </div>`
-        containerProdructos.innerHTML += fragmentItemProduct
+  containerProdructos.innerHTML += fragmentItemProduct
 
-      })
+})
 
-      
-/*
-      function addProductCard(id) {
 
-        
+// Operamos con los botonos para agregar al carrito de compras
+let btnAddCar = document.querySelectorAll('.btn-add-card')
+btnAddCar = [...btnAddCar]
 
-        const itemProduct = productos.find(prod => prod.id===id)
-        carShopping.push(itemProduct) 
-        
-        
-        mostrarProductosCarShopping()
-      } */
+btnAddCar.forEach(btn => {
+  btn.addEventListener('click', e => {
+    //Treemos el Id del Boton
+    let idButton = parseInt(e.target.id)
+    addProductCard(idButton)
+   
 
-      function addProductCard(id) {
+  })
+})
 
-        //som es solo para true o false
-        //const existe = carShopping.some(prod => prod.id ===id)
-        
-        const existe = carShopping.find(prod => prod.id ===id)
-       // let producSelected = carShopping.find(productos => productos.id===id)
+function addProductCard(id) {
 
-        if (existe) {
-          
-          let index = carShopping.indexOf(existe)
-          carShopping[index].quantitySelected++
-         // carShopping[index].quantitySelected++
-        
-       
-        } else {
+  //som es solo para true o false
+  //const existe = carShopping.some(prod => prod.id ===id)
+  const existe = carShopping.find(prod => prod.id === id)
+  if (existe) {
+    let index = carShopping.indexOf(existe)
+    carShopping[index].quantitySelected++
+    // carShopping[index].quantitySelected++
+  } else {
+    const item = productos.find(item => item.id === id)
+    item.quantitySelected = 1
+    carShopping.push(item)
+  }
 
-          const item = productos.find(item => item.id===id)
-          item.quantitySelected = 1
-          carShopping.push(item)
-           
-        }
-        mostrarProductosCarShopping()
-       console.log(carShopping);
-      }
+  mostrarProductosCarShopping()
+ 
+}
 
 
 
-      // CARRITO DE COMPRA 
+
+// CARRITO DE COMPRA 
 
 
-      const mostrarProductosCarShopping =()=>{
-        const layoutCarShopping = document.querySelector('#container-productos-add')
+const mostrarProductosCarShopping = () => {
+  const layoutCarShopping = document.querySelector('#container-productos-add')
 
-        layoutCarShopping.innerHTML = ''
-        
-        carShopping.forEach(prod =>{
+  layoutCarShopping.innerHTML = ''
 
-          const {id,name,price,image,quantitySelected,quantity} = prod
+  carShopping.forEach(prod => {
 
-          const fragmentCarShopping = `<div class="container-card-shopp">
+    const { id, name, price, image, quantitySelected, quantity } = prod
+
+    const fragmentCarShopping = `
+          <div class="container-card-shopp">
           <figure class="container-card-imag-shopp">
               <img src="${image}" alt="" class="imag-product-card-shopp">
           </figure>
@@ -291,9 +288,9 @@
               </div>
               <h3 class="sub-total-produc"></h3>
               <div class="container-cant-produc">
-                  <button onclick="btnDismin(${id})" class="btn-cantidad">-</button>
-                  <h3 class="unidad-produc">${quantitySelected} units</h3>
-                  <button onclick="btnAument(${id})" class="btn-cantidad">+</button>
+                  <button id="${id}" class="btn-cantidad btn-menos">-</button>
+                  <h3 class="unidad-produc">${quantitySelected}</h3>
+                  <button id="${id}" class="btn-cantidad btn-mas">+</button>
               </div>
           </article>
           <div class="container-btn-remover">
@@ -301,94 +298,137 @@
               </div>
       </div> `
 
-            layoutCarShopping.innerHTML += fragmentCarShopping
+    layoutCarShopping.innerHTML += fragmentCarShopping
 
-        })
+    getTotal()
+    
+  })
 
-        if (carShopping.length===0) {
+  if (carShopping.length === 0) {
 
-         layoutCarShopping.innerHTML = `
+    layoutCarShopping.innerHTML = `
          <figure id="imagen-car-shopping" class="imag-shopping">
                <img src="./assets/images/empty-cart.png" alt="">
-         </figure> ` 
-        }
-        
+         </figure> `
+  }
 
-        //Contador 
-        cantidadProduCar.textContent = carShopping.length
 
-        precioTotal.textContent = 'price * carShopping.length'
-        
-        guardarStorage()
+  //Contador  del carrito General
+  cantidadProduCar.textContent = carShopping.length
+
+  btnAumentarCantadadProducto()
+
+  guardarStorage()
+}
+
+function getTotal() {
+  let sumaTotal  = 0
+  let total = carShopping.reduce((sum,item)=>{
+    sumaTotal = sum + item.quantitySelected*item.price
+    return sumaTotal
+  },0)
+  precioTotal.innerText = `$${total}.00`
+}
+
+
+
+/////Me qude aqui
+
+
+
+function btnAumentarCantadadProducto() {
+  let btnMas = document.querySelectorAll('.btn-menos')
+  btnMas = [...btnMas]
+    
+  btnMas.forEach(btmenos =>{
+    btmenos.addEventListener('click', e =>{
+      const idBtnmas = parseInt(e.target.id)
+      let cantidadItem =parseInt(e.target.nextElementSibling.innerText)
+
+      let productoFiltrado = carShopping.find(prod => prod.id === idBtnmas)
+    
+      if (productoFiltrado.id==idBtnmas && productoFiltrado.quantitySelected>0) {
+         productoFiltrado.quantitySelected = cantidadItem       
       }
-
-
-      //ME QUEDE AQUI ---- 9/12/22
       
-      function btnDismin(id) {
+  
 
-        console.log(id);
-        carShopping.forEach(prod => {
-           if (prod.id===id) {
-           
-           }
-        })
-        
-      }
+      console.log(productoFiltrado);
 
-      function btnAument(id) {
-        
-      }
-
-      function elimarProductoCar(id) {
-        const juegoId = id
-        
-        carShopping = carShopping.filter(prod => prod.id !==juegoId)
-        mostrarProductosCarShopping()
-      }
-
-      function guardarStorage() {
-        localStorage.setItem('carrito', JSON.stringify(carShopping))
-      }
+      getTotal()
+      
+    })
+  
+  })
+}
 
 
 
 
-      /* -------- ADD PRODUCTS TO CAR------- */
+//ME QUEDE AQUI ---- 9/12/22
+let contadorS = 1
+function btnDismin() {
 
-      /*
+  
+  
 
 
-      function addProductCard(id) {
-
-        const itemProduct = productos.find(prod => prod.id===id)
-        carShopping.push(itemProduct)
-        mostrarProductosCarShopping()
-      }
+}
 
 
 
 
-      const productoEnElCar = []
-      function addProductCard(id) {
 
-        let producSelected = carShopping.find(productos => productos.id===itemId)
 
-        if (producSelected) {
-          
-          let index = carShopping.indexOf(producSelected)
-          carShopping[index].quantitySelected++
+function elimarProductoCar(id) {
+  const juegoId = id
 
-        } else {
+  carShopping = carShopping.filter(prod => prod.id !== juegoId)
+  mostrarProductosCarShopping()
+}
 
-          const item = productsItem.find(item => item.id===itemId)
-          item.quantitySelected = 1
-          carShopping.push(item)
-        }
-        
-        productosAddCarShopping(carShopping)
-        console.log(carShopping);
-      }*/
+function guardarStorage() {
+  localStorage.setItem('carrito', JSON.stringify(carShopping))
+}
+
+
+
+
+/* -------- ADD PRODUCTS TO CAR------- */
+
+/*
+
+
+function addProductCard(id) {
+
+  const itemProduct = productos.find(prod => prod.id===id)
+  carShopping.push(itemProduct)
+  mostrarProductosCarShopping()
+}
+
+
+
+
+const productoEnElCar = []
+function addProductCard(id) {
+
+  let producSelected = carShopping.find(productos => productos.id===itemId)
+
+  if (producSelected) {
+    
+    let index = carShopping.indexOf(producSelected)
+    carShopping[index].quantitySelected++
+
+  } else {
+
+    const item = productsItem.find(item => item.id===itemId)
+    item.quantitySelected = 1
+    carShopping.push(item)
+  }
+  
+  productosAddCarShopping(carShopping)
+  console.log(carShopping);
+}*/
 
 
 
